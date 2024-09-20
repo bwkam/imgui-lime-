3f3257840badcd07ae9e3bf77e396e5eaa3ac4b1
bwkam <userdev987@gmail.com>
2024-09-20 22:16:26 +0300

    that works

--- src/Main.hx
+++ src/Main.hx
@@ -7,16 +7,7 @@ import imgui.ImGui;
 import imgui.Helpers.*;
 
 class Main extends Application {
-	static var ImGui_Impl(get, never):Dynamic;
-
-	inline static function get_ImGui_Impl():Dynamic
-		return untyped window.ImGui_Impl;
-
-	static var io:ImGuiIO = null;
-
 	var init:Bool = false;
-
-	static var framePending:Bool = false;
 
 	public function new() {
 		super();
@@ -24,7 +15,6 @@ class Main extends Application {
 	}
 
 	public function initialize(done:() -> Void, canvas:Canvas2DRenderContext):Void {
-		trace("initting");
 		if (preloader.complete) {
 			loadImGui(done, canvas);
 			init = true;
@@ -56,48 +46,9 @@ class Main extends Application {
 	static function loadImGui(done:() -> Void, canvas:Canvas2DRenderContext) {
 		loadScript('assets/imgui.umd.js', function(_) {
 			loadScript('assets/imgui_impl.umd.js', function(_) {
-				Reflect.field(untyped window.ImGui, 'default')().then(function() {
-					initImGui(done, canvas);
-				}, function() {
-					trace('Failed to load ImGui bindings');
-				});
+				trace("finished loading scripts");
 			});
 		});
-	}
-
-	public static function newFrame():Void {
-		ImGui_Impl.NewFrame(haxe.Timer.stamp() * 1000);
-		ImGui.newFrame();
-
-		framePending = true;
-	}
-
-	public static function endFrame():Void {
-		if (!framePending)
-			return;
-		framePending = false;
-
-		ImGui.endFrame();
-		ImGui.render();
-
-		ImGui_Impl.RenderDrawData(ImGui.getDrawData());
-
-		// clay.Clay.app.runtime.skipKeyboardEvents = io.wantCaptureKeyboard;
-		// clay.Clay.app.runtime.skipMouseEvents = io.wantCaptureMouse;
-	}
-
-	static function initImGui(done:() -> Void, canvas:Canvas2DRenderContext) {
-		ImGui.createContext();
-		ImGui.styleColorsDark();
-		ImGui_Impl.Init(canvas);
-
-		io = ImGui.getIO();
-
-		// Graphics.bindTexture2d(io.fonts.texID);
-		// Graphics.setTexture2dMagFilter(NEAREST);
-		// Graphics.setTexture2dMinFilter(NEAREST);
-
-		done();
 	}
 
 	public override function render(context:RenderContext):Void {
